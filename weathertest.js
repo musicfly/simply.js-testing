a45d0021a1b5635e163472a47c2a2c070f29ee98
musicfly <edpocock@gmail.com>
2015-03-08 11:05:05 +0000

    Update weathertest.js

--- weathertest.js
+++ weathertest.js
@@ -8,7 +8,7 @@ simply.on('singleClick', function(e) {
      var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
       'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
      ajax({ url: weatherUrl, type: 'json' }, function(data) {
-      simply.text({ title: "The temperature for " + data.name + " is " + Math.round(data.main.temp) + "C,", subtitle: " outlook " + data.weather[0].description});
+      simply.text({ title: "Weather for " + data.name + " is " + Math.round(data.main.temp) + "C,", subtitle: " expect " + data.weather[0].description});
   });
   
 });
