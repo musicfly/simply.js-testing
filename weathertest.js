deacef9e274f4a7c3ecbc5d67ee96f95b5296d19
musicfly <edpocock@gmail.com>
2015-03-08 08:55:24 +0000

    Update weathertest.js

--- weathertest.js
+++ weathertest.js
@@ -1,13 +1,12 @@
 simply.vibe('short');
-simply.title("Today's weather"); 
-simply.text("Today's weather is "); 
+simply.title("Today's temperature for"); 
+
 
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
   var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
       'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({ title: data.name, subtitle: data.main.temp });
+    simply.text({ title: data.name + "is", subtitle: data.main.temp, body: "degrees C"});
   });
 });
-
