49f3b205023b26f9cfd743f1b436f71aea9de62f
musicfly <edpocock@gmail.com>
2015-03-08 07:54:29 +0000

    Create weathertest.js

---
+++ weathertest.js
@@ -0,0 +1,12 @@
+
+simply.vibe('short');
+
+navigator.geolocation.getCurrentPosition(function(pos) {
+  var coords = pos.coords;
+  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  ajax({ url: weatherUrl, type: 'json' }, function(data) {
+    simply.text({ title: data.name, subtitle: data.main.temp });
+  });
+});
+
