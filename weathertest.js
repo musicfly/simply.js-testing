95a9690e0a68786d9b1cf7d551ec3a49e6a14644
musicfly <edpocock@gmail.com>
2015-03-08 09:28:19 +0000

    Update weathertest.js

--- weathertest.js
+++ weathertest.js
@@ -7,7 +7,7 @@ navigator.geolocation.getCurrentPosition(function(pos) {
   var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
       'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({ title: "The temp for " + data.name + " is ", subtitle:  Math.round(data.main.temp) + "C"});
+    simply.text({ title: "The temp for " + data.name + " is ", subtitle:  Math.round(data.main.temp) + "C " + data.weather.description});
   });
 });
 
